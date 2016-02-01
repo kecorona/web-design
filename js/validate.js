5a834d4f39f777b0df8ddb2c4454502400c25c24
Kristin Corona <kristincorona@gmail.com>
2016-02-01 09:22:27 -0500

    Initial commit

---
+++ js/validate.js
@@ -0,0 +1,36 @@
+/* <![CDATA[ */
+$('#contactform').submit(function(){
+
+		var action = $(this).attr('action');
+
+		$("#message-contact").slideUp(750,function() {
+		$('#message-contact').hide();
+
+ 		$('#submit-contact')
+			.after('<img src="img/ajax-loader2.gif" class="loader" >')
+			.attr('disabled','disabled');
+
+		$.post(action, {
+			name_contact: $('#name_contact').val(),
+			lastname_contact: $('#lastname_contact').val(),
+			email_contact: $('#email_contact').val(),
+			phone_contact: $('#phone_contact').val(),
+			message_contact: $('#message_contact').val(),
+			verify_contact: $('#verify_contact').val()
+		},
+			function(data){
+				document.getElementById('message-contact').innerHTML = data;
+				$('#message-contact').slideDown('slow');
+				$('#contactform img.loader').fadeOut('slow',function(){$(this).remove()});
+				$('#submit-contact').removeAttr('disabled');
+				if(data.match('success') != null) $('#contactform').slideUp('slow');
+
+			}
+		);
+
+		});
+
+		return false;
+
+	});
+  /* ]]> */
